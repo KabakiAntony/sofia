d587dc974f3bbcff1ea2d2897318957b85326ded
KabakiAntony <kabaki.antony@gmail.com>
2022-08-16 16:35:45 +0300

    created the forms file to form a base for the signup form

---
+++ accounts/forms.py
@@ -0,0 +1,7 @@
+from .models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['first_name','last_name','email','password1','password2']
