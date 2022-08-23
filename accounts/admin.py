bb6308af3b510f4d7d43217863641c399f6750ad
KabakiAntony <kabaki.antony@gmail.com>
2022-08-23 20:50:08 +0300

    removed admin from accounts module to the common admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,12 +1,2 @@
-from django.contrib import admin
-from .models import User
+# from django.contrib import admin
 
-
-class JournalingAdmin(admin.AdminSite):
-    site_header = 'Journaling Administration'
-    site_title = 'Journaling Admin'
-    index_title = 'Journaling Admin'
-
-admin_site = JournalingAdmin(name='admin')
-
-admin_site.register(User)
