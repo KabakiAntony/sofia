2b77502dbcacd1f46b8da212e583174b17e27f53
KabakiAntony <kabaki.antony@gmail.com>
2022-08-24 20:09:31 +0300

    created a search view

--- products/views.py
+++ products/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import Product, Category
+from django.db.models import Q
 
 
-def home_page(request):
+def home(request):
     """ this will show products on the homepage """
     products = Product.objects.all()
     categories = Category.objects.all()
@@ -20,5 +21,17 @@ def product_details(request, slug):
         "product":product
     }
     return render(request, "products/product_detail.html", context)
+
+def search(request):
+    """ show the results of a product that a user has searched for """
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    products = Product.objects.filter(
+        Q(name__contains=q)|
+        Q(description__icontains=q)|
+        Q(category__name__icontains=q))
+    context = {
+        "products":products,
+    }
+    return render(request, "products/search.html", context)
 
 
