d062cfa27c9a4d7bbd6b415d245262083d2af785
KabakiAntony <kabaki.antony@gmail.com>
2022-08-30 19:39:44 +0300

    added a return and a render for the number of items in the cart so that it can be visible on all pages

--- products/views.py
+++ products/views.py
@@ -2,30 +2,39 @@ from django.shortcuts import render
 from .models import Product, Category
 from django.db.models import Q
 from django.contrib.postgres.search import SearchVector
+from cart.utils import cart_data
 
 
 
 def home(request):
     """ this will show products on the homepage """
+    data = cart_data(request)
+    cartItems = data['cartItems']
     products = Product.objects.all()
     categories = Category.objects.all()
     context = {
         "categories":categories,
         "products":products,
+        'cartItems':cartItems
     }
     return render(request, "products/index.html", context)
 
 
 def product_details(request, slug):
     """ Show the details of an individual product"""
+    data = cart_data(request)
+    cartItems = data['cartItems']
     product = Product.objects.get(slug=slug)
     context = {
-        "product":product
+        "product":product,
+        'cartItems':cartItems,
     }
     return render(request, "products/product_detail.html", context)
 
 def search(request):
     """ show the results of a product that a user has searched for """
+    data = cart_data(request)
+    cartItems = data['cartItems']
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     # below is an implementation of fulltext search
     # instead of using Q lookup which is limited to one word matches
@@ -34,6 +43,7 @@ def search(request):
         search=SearchVector("name", "description", "category")).filter(search=q)
     context = {
         "products":products,
+        'cartItems':cartItems,
     }
     return render(request, "products/search.html", context)
 
