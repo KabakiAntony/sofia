a7220987e1856b2070fbc72a70b336a833c1d8de
KabakiAntony <kabaki.antony@gmail.com>
2022-09-20 16:13:31 +0300

    added cart data to the accounts view for unsigned in user so it will show the items they have on cart even as guest users

--- accounts/views.py
+++ accounts/views.py
@@ -3,10 +3,14 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from .forms import MyUserCreationForm
 from .models import User,Customer
+from cart.utils import cart_data
 
 
 def signup_user(request):
     form = MyUserCreationForm()
+    data = cart_data(request)
+    cartItems = data['cartItems']
+    
 
     if request.method == 'POST':
         form = MyUserCreationForm(request.POST)
@@ -25,10 +29,12 @@ def signup_user(request):
         else:
             messages.error(request, "An error occurred during registration")
 
-    context = {'form': form }
+    context = {'form': form, 'cartItems':cartItems}
     return render(request, 'accounts/signup.html', context)
 
 def signin_user(request):
+    data = cart_data(request)
+    cartItems = data['cartItems']
     # later in dev add a next link to always take the user back to the
     # view that required him to be logged in.
     if request.user.is_authenticated:
@@ -55,8 +61,8 @@ def signin_user(request):
             messages.error(request, 
             'User account could not be found, please signup to continue.')
         
-
-    return render(request, 'accounts/signin.html')
+    context = {'cartItems':cartItems}
+    return render(request, 'accounts/signin.html', context)
 
 def signout_user(request):
     logout(request)
