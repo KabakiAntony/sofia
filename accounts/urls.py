9b13afdd3325a3da287d31ce99348c4be934f132
KabakiAntony <kabaki.antony@gmail.com>
2022-09-29 21:02:31 +0300

    add email activation url

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('signup/', views.signup_user, name="signup"),
     path('signin/', views.signin_user, name="signin"),
     path('signout/', views.signout_user, name='signout'),
+    path('verify/<uidb64>/<token>/', views.verify_email, name='verify'),
 ]
