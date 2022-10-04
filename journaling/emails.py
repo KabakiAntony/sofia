341ef4d13b3d1c51e10fe9476aa6e671477e3068
KabakiAntony <kabaki.antony@gmail.com>
2022-10-04 16:01:05 +0300

    change from using inbuilt django send email function to using sendmail function

--- journaling/emails.py
+++ journaling/emails.py
@@ -1,16 +1,19 @@
-import os
-from django.core.mail import EmailMessage
 from django.contrib import messages
 from journaling.settings import env
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
 
 def send_email(request, receiver_email, the_subject, the_content):
     """send email on relevant user action"""
-    if receiver_email and the_subject and the_content:
-        try:
-            subject, from_email, to = the_subject, env('FROM_EMAIL'), receiver_email
-            html_content = the_content
-            message = EmailMessage(subject, html_content, from_email, [to])
-            message.content_subtype = "html"
-            message.send()
-        except Exception as e:
+    message = Mail(
+        from_email=(env('FROM_EMAIL'), "Journaling Therapy"),
+        to_emails=receiver_email,
+        subject=the_subject,
+        html_content=the_content,
+    )
+    try:
+        sg = SendGridAPIClient(env("SENDGRID_API_KEY"))
+        sg.send(message)
+    except Exception as e:
             return messages.error(request, f"The following error occured, {e}")
