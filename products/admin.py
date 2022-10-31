11127fa2c0d4bcec501f85cc39c1c84fc8cba277
KabakiAntony <kabaki.antony@gmail.com>
2022-10-31 16:01:19 +0300

    add custom product app models classes and registered them

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from journaling.admin import journaling_admin_site
+from .models import Product, Category, ProductImage
 
-# Register your models here.
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+
+
+class ProductAdmin(admin.ModelAdmin):
+    # define how the custom form is going to be rendered
+    list_display = ['id','name','description','category','price', 'stock', 'available', 'date']
+    list_display_links = ['name']
+    list_filter = ['category']
+    inlines = [ProductImageInline]
+
+
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ['id','product','thumb']
+    readonly_fields = ['thumb']
+    list_display_links = ['product']
+    list_filter = ['product']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id','name','description']
+    list_display_links = ['name']
+
+journaling_admin_site.register(Product,ProductAdmin)
+journaling_admin_site.register(ProductImage,ProductImageAdmin)
+journaling_admin_site.register(Category, CategoryAdmin)
