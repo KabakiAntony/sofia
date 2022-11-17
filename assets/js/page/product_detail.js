813703057789c2a06fa1b4cdaf554c995a3bb52c
KabakiAntony <kabaki.antony@gmail.com>
2022-11-17 12:21:55 +0300

    add  script for scrolling images in the courasel

--- assets/js/page/product_detail.js
+++ assets/js/page/product_detail.js
@@ -1,10 +1,23 @@
-let main_image = document.getElementById("main-image");
-let other_images = document.getElementsByClassName("other-image");
-let domain = window.location
+let slideIndex = 1;
+showSlides(slideIndex);
 
-for (let i=0; i< other_images.length; i++){
-    other_images[i].addEventListener('click', function(){
-    let image_src = this.dataset.other_image_src;
-    main_image.src = `${domain.origin}${image_src}`;
-  })
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+} 
