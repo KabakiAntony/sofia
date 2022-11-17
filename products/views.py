1e89293718960093b4f6649db72151e742ecaf3c
KabakiAntony <kabaki.antony@gmail.com>
2022-11-17 12:25:35 +0300

    add context variable for returning similar and products that match well with the product in question

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Product, Category
 from django.db.models import Q
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery
 from cart.utils import cart_data
 
 
@@ -12,10 +12,11 @@ def home(request):
     cartItems = data['cartItems']
     products = Product.objects.all()
     categories = Category.objects.all()
+
     context = {
         "categories":categories,
         "products":products,
-        'cartItems':cartItems
+        'cartItems':cartItems,
     }
     return render(request, "products/index.html", context)
 
@@ -26,10 +27,15 @@ def product_details(request, slug):
     cart = data['cart']
     cartItems = data['cartItems']
     product = Product.objects.get(slug=slug)
+    similar_products = Product.objects.filter(category=product.category).exclude(slug__iexact=slug)[:6]
+    goes_well_with = product.following.all()
+    
     context = {
         "product":product,
+        "similar_products":similar_products,
         'cartItems':cartItems,
         "cart":cart,
+        "goes_well_with":goes_well_with,
     }
     return render(request, "products/product_detail.html", context)
 
@@ -42,7 +48,7 @@ def search(request):
     # instead of using Q lookup which is limited to one word matches
     # ofcourse fulltext search has its limitation slows down the db with heavy usage
     products = Product.objects.annotate(
-        search=SearchVector("name", "description", "category")).filter(search=q)
+        search=SearchVector("name", "description", "category")).filter(search=SearchQuery(q))
     context = {
         "products":products,
         'cartItems':cartItems,
