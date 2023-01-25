f84a334af054428e2c937e542395ef4faa68dd72
KabakiAntony <kabaki.antony@gmail.com>
2023-01-25 12:21:40 +0300

    add a view for ajax call, refined search view to get the title of the category a user has searched

--- products/views.py
+++ products/views.py
@@ -1,8 +1,11 @@
+import json
 from django.shortcuts import render
-from .models import Product, Category
+from .models import Product, Category, Product_Entry
 from django.db.models import Q
 from django.contrib.postgres.search import SearchVector, SearchQuery
 from cart.utils import cart_data
+from django.http import JsonResponse
+from django.template.loader import render_to_string 
 
 
 
@@ -29,9 +32,11 @@ def product_details(request, slug):
     product = Product.objects.get(slug=slug)
     similar_products = Product.objects.filter(category=product.category).exclude(slug__iexact=slug)[:6]
     goes_well_with = product.following.all()
-    
+    entries = product.product_entry_set.all()
+
     context = {
         "product":product,
+        "entries": entries,
         "similar_products":similar_products,
         'cartItems':cartItems,
         "cart":cart,
@@ -44,14 +49,30 @@ def search(request):
     data = cart_data(request)
     cartItems = data['cartItems']
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    # below is an implementation of fulltext search
-    # instead of using Q lookup which is limited to one word matches
-    # ofcourse fulltext search has its limitation slows down the db with heavy usage
+  
     products = Product.objects.annotate(
-        search=SearchVector("name", "description", "category")).filter(search=SearchQuery(q))
+        search=SearchVector("title", "description", "category__title")).filter(search=SearchQuery(q))
+
     context = {
         "products":products,
         'cartItems':cartItems,
     }
     return render(request, "products/search.html", context)
+
+
+def get_product_entry(request):
+    data = {}
+    if request.method == 'POST':
+        sku = json.load(request)['sku']
+
+        entry = Product_Entry.objects.get(sku=sku)
+        product = Product.objects.get(id=entry.product.id)
+
+        context = {
+            'entry': entry,
+            "product":product,
+        }
+        data = {'rendered_product': render_to_string('products/entry_detail.html', context=context)}
+        
+    return JsonResponse(data)
 
