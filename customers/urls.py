f4794632513ee2218a6b95fb47c612941106d511
KabakiAntony <kabaki.antony@gmail.com>
2023-01-26 11:09:27 +0300

    add customers app

---
+++ customers/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .import views
+
+app_name = 'customers'
+
+urlpatterns = [
+    # path('', views.home, name="list"),
+    # path('search/', views.search, name="search"),
+    # path('select/', views.get_product_entry, name="selected_product"),
+    # path('<slug:slug>/', views.product_details, name="product_detail"),
+]
