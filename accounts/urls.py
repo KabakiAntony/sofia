cb1488f1fe41ff445e7e6f464d51ec3f4553a1c3
Kabaki Antony <kabaki.antony@gmail.com>
2023-10-06 11:47:24 +0300

    set the type for expected params for verify and change urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .import views 
+from .import views
 
 urlpatterns = [
     path('signup/', views.signup_user, name="signup"),
     path('signin/', views.signin_user, name="signin"),
     path('signout/', views.signout_user, name='signout'),
-    path('verify/<uidb64>/<token>/', views.verify_email, name='verify'),
-    path('forgot/', views.send_reset_link, name='forgot' ),
-    path('change/<uidb64>/<token>/', views.change_password, name='change'),
+    path('verify/<str:uidb64>/<str:token>/',
+         views.verify_email, name='verify'),
+    path('forgot/', views.send_reset_link, name='forgot'),
+    path('change/<str:uidb64>/<str:token>/',
+         views.change_password, name='change'),
 ]
