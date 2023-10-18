5ce73a54f545ed30a98a7807bc82f40abca477c5
Kabaki Antony <kabaki.antony@gmail.com>
2023-10-18 10:50:27 +0300

    remove orders

--- customers/urls.py
+++ customers/urls.py
@@ -6,7 +6,6 @@ app_name = 'customers'
 urlpatterns = [
     path('profile/', views.user_profile, name="profile"),
     path('address/', views.add_address, name="address"),
-    path('orders/', views.get_orders, name="orders"),
     path('areas/', views.get_areas, name='areas'),
     path('address/update/', views.update_address, name="update_address"),
     path('get-shipping-cost/', views.get_shipping_cost, name="get-shipping-cost"),
